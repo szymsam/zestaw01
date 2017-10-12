6374bef4ef35b8b2c28b1e5a69939260d5febd07
Szymon Samólnik <szymon.samolnik@gmail.com>
2017-10-12 18:02:00 +0200

    work in progress-zadanie5

---
+++ zad05/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    char a;
+    char b;
+    char c;
+    char d;
+
+    printf("podaj literę1: ");
+    scanf(" %c", &a);
+
+    printf("podaj literę2: ");
+    scanf(" %c", &b);
+
+    printf("podaj literę3: ");
+    scanf(" %c", &c);
+
+    printf("podaj literę4: ");
+    scanf(" %c", &d);
+
+    return 0;
+}
